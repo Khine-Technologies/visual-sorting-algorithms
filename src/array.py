3811be6352203dba8b9e39213d75c79def70b826
williamKhine <williamKhine@users.noreply.github.com>
2022-08-09 03:46:27 +0630

    General array manipulation functions

--- src/array.py
+++ src/array.py
@@ -1,9 +1,13 @@
 import random
 
 
-def make(arr_len: int, low: int, high: int) -> list[int]:
+def make_array(arr_len: int, low: int, high: int) -> list[int]:
     arr = []
     for i in range(arr_len):
         arr.append(random.randint(low, high))
 
     return arr
+
+
+def swap(arr: list[int], x: int, y: int) -> None:
+    arr[x], arr[y] = arr[y], arr[x]
